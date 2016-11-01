0557f16876c1a87bccbeeaf4151947ce561b0f45
MrGupta <purus.gupta@gmail.com>
2016-11-01 22:15:37 +0530

    Ready for submission

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/touch_helper/OnStartDragListener.java
@@ -0,0 +1,12 @@
+package com.sam_chordas.android.stockhawk.touch_helper;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by MrGupta on 01-Nov-16.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
+
